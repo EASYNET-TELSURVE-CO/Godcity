ad747455efaf9a2407094ce47dfb87265c95f0a1
Dr-dyrane <halodyrane@gmail.com>
2024-07-21 18:39:11 +0100

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,3 @@
-// src/pages/Home.jsx
 import React, { useEffect } from "react";
 import { IconButton } from "@mui/material";
 import PersonIcon from "@mui/icons-material/Person";
@@ -7,6 +6,8 @@ import AttachMoneyIcon from "@mui/icons-material/AttachMoney";
 import EmailIcon from "@mui/icons-material/Email";
 import { useAuth } from "../contexts/AuthContext";
 import { useNavigate } from "react-router";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 const Home = () => {
     const { user } = useAuth();
@@ -18,73 +19,83 @@ const Home = () => {
         }
     }, [user, navigate]);
 
+    useEffect(() => {
+        AOS.init({ duration: 1000 });
+    }, []);
+
     return (
         <div>
             <section
                 className="flex flex-col bg-hero dark:bg-hero_dark bg-cover min-h-screen dark:text-contrast text-center
-			space-y-4 xs:space-y-6 sm:space-y-8 md:space-y-10 lg:space-y-12 xl:space-y-14 2xl:space-y-16 justify-center items-center py-20"
+                space-y-4 xs:space-y-6 sm:space-y-8 md:space-y-10 lg:space-y-12 xl:space-y-14 2xl:space-y-16 justify-center items-center py-20"
             >
                 <div className="bg-white/25 dark:bg-black/25 m-4 p-8 rounded-3xl bg-opacity-25 text-[#000] dark:text-white">
-                    <h1 className="text-3xl xs:text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl 2xl:text-9xl font-bold mb-4">
+                    <h1 className="text-3xl xs:text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl 2xl:text-9xl font-bold mb-4" data-aos="fade-up">
                         Welcome to GodCi†y
                     </h1>
-                    <p className="text-base xs:text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl 2xl:text-5xl mb-8">
+                    <p className="text-base xs:text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl 2xl:text-5xl mb-8" data-aos="fade-up" data-aos-delay="100">
                         Unify. Serve. Inspire.
                     </p>
-                    <p className="p-10 text-lg xs:text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl 2xl:text-6xl italic relative z-10 font-light">
+                    <p className="p-10 text-lg xs:text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl 2xl:text-6xl italic relative z-10 font-light" data-aos="fade-up" data-aos-delay="200">
                         "For where two or three gather in my name, there am I
                         with them." – Matthew 18:20
                     </p>
-                    <button className="bg-primary text-white text-sm xs:text-base sm:text-lg md:text-xl lg:text-2xl xl:text-3xl 2xl:text-4xl px-4 xs:px-6 sm:px-8 md:px-10 lg:px-12 xl:px-14 2xl:px-16 py-2 xs:py-3 sm:py-4 md:py-5 lg:py-6 xl:py-7 2xl:py-8 rounded-full hover:bg-primary-dark transition">
+                    <button className="bg-primary text-white text-sm xs:text-base sm:text-lg md:text-xl lg:text-2xl xl:text-3xl 2xl:text-4xl px-4 xs:px-6 sm:px-8 md:px-10 lg:px-12 xl:px-14 2xl:px-16 py-2 xs:py-3 sm:py-4 md:py-5 lg:py-6 xl:py-7 2xl:py-8 rounded-full hover:bg-primary-dark transition" data-aos="fade-up" data-aos-delay="300">
                         Get Started
                     </button>
                 </div>
             </section>
-<section className="bg-background dark:bg-background-dark py-20">
-    <div className="container mx-auto px-4">
-        <h2 className="text-3xl font-bold text-center mb-10">
-            Features
-        </h2>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
-            <FeatureCard
-                icon={PersonIcon}
-                title="Member Management"
-                description="Efficiently manage your church members with detailed profiles and group classifications."
-            />
-            <FeatureCard
-                icon={EventIcon}
-                title="Event Management"
-                description="Organize and manage church events seamlessly, including event scheduling, registration management, and attendance tracking."
-            />
-            <FeatureCard
-                icon={AttachMoneyIcon}
-                title="Financial Management"
-                description="Keep track of donations and finances with detailed financial records, reporting capabilities, and online payment integrations."
-            />
-            <FeatureCard
-                icon={EmailIcon}
-                title="Communication"
-                description="Communicate effectively with members through email and SMS notifications, newsletters, and discussion forums."
-            />
-        </div>
-    </div>
-</section>
+
+            <section className="bg-background dark:bg-background-dark py-20">
+                <div className="container mx-auto px-4">
+                    <h2 className="text-3xl font-bold text-center mb-10" data-aos="fade-up">
+                        Features
+                    </h2>
+                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
+                        <FeatureCard
+                            icon={PersonIcon}
+                            title="Member Management"
+                            description="Efficiently manage your church members with detailed profiles and group classifications."
+                            aos="fade-up"
+                        />
+                        <FeatureCard
+                            icon={EventIcon}
+                            title="Event Management"
+                            description="Organize and manage church events seamlessly, including event scheduling, registration management, and attendance tracking."
+                            aos="fade-up"
+                        />
+                        <FeatureCard
+                            icon={AttachMoneyIcon}
+                            title="Financial Management"
+                            description="Keep track of donations and finances with detailed financial records, reporting capabilities, and online payment integrations."
+                            aos="fade-up"
+                        />
+                        <FeatureCard
+                            icon={EmailIcon}
+                            title="Communication"
+                            description="Communicate effectively with members through email and SMS notifications, newsletters, and discussion forums."
+                            aos="fade-up"
+                        />
+                    </div>
+                </div>
+            </section>
         </div>
     );
 };
 
-const FeatureCard = ({ icon: Icon, title, description }) => {
+const FeatureCard = ({ icon: Icon, title, description, aos }) => {
     return (
-        <div className="bg-none p-8 rounded-2xl shadow-lg text-center flex flex-col justify-between">
-            <IconButton className="text-4xl mb-4 mx-auto">
+        <div className="bg-none p-8 rounded-2xl shadow-lg  text-center flex flex-col justify-between transform transition-transform duration-300 hover:scale-105 hover:shadow-xl dark:hover:shadow-white/40" data-aos={aos}>
+            <IconButton aria-label={title} className="text-4xl mb-4 mx-auto">
                 <Icon fontSize="large" style={{ color: "#c0a700" }} />
             </IconButton>
             <h3 className="text-xl text-contrast dark:text-contrast-dark font-semibold mb-2">
                 {title}
             </h3>
-            <p className="text-gray-600 dark:text-gray-400 font-light">
+            <p className="text-gray-600 dark:text-gray-400 font-light mb-4">
                 {description}
             </p>
+            <a href="#" className="text-primary dark:text-primary-dark hover:underline">Learn More</a>
         </div>
     );
 };
