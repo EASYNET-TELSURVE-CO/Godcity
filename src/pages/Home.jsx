45fc101a929f4935b5a397020ad725244965286b
Dr-dyrane <halodyrane@gmail.com>
2024-07-21 18:54:13 +0100

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,11 @@
+// src/pages/Home.jsx
+
 import React, { useEffect } from "react";
-import { IconButton } from "@mui/material";
-import PersonIcon from "@mui/icons-material/Person";
-import EventIcon from "@mui/icons-material/Event";
-import AttachMoneyIcon from "@mui/icons-material/AttachMoney";
-import EmailIcon from "@mui/icons-material/Email";
 import { useAuth } from "../contexts/AuthContext";
 import { useNavigate } from "react-router";
 import AOS from 'aos';
 import 'aos/dist/aos.css';
+import Features from "../components/home/Features";
 
 const Home = () => {
     const { user } = useAuth();
@@ -46,56 +44,7 @@ const Home = () => {
                 </div>
             </section>
 
-            <section className="bg-background dark:bg-background-dark py-20">
-                <div className="container mx-auto px-4">
-                    <h2 className="text-3xl font-bold text-center mb-10" data-aos="fade-up">
-                        Features
-                    </h2>
-                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
-                        <FeatureCard
-                            icon={PersonIcon}
-                            title="Member Management"
-                            description="Efficiently manage your church members with detailed profiles and group classifications."
-                            aos="fade-up"
-                        />
-                        <FeatureCard
-                            icon={EventIcon}
-                            title="Event Management"
-                            description="Organize and manage church events seamlessly, including event scheduling, registration management, and attendance tracking."
-                            aos="fade-up"
-                        />
-                        <FeatureCard
-                            icon={AttachMoneyIcon}
-                            title="Financial Management"
-                            description="Keep track of donations and finances with detailed financial records, reporting capabilities, and online payment integrations."
-                            aos="fade-up"
-                        />
-                        <FeatureCard
-                            icon={EmailIcon}
-                            title="Communication"
-                            description="Communicate effectively with members through email and SMS notifications, newsletters, and discussion forums."
-                            aos="fade-up"
-                        />
-                    </div>
-                </div>
-            </section>
-        </div>
-    );
-};
-
-const FeatureCard = ({ icon: Icon, title, description, aos }) => {
-    return (
-        <div className="bg-none p-8 rounded-2xl shadow-lg  text-center flex flex-col justify-between transform transition-transform duration-300 hover:scale-105 hover:shadow-xl dark:hover:shadow-white/40" data-aos={aos}>
-            <IconButton aria-label={title} className="text-4xl mb-4 mx-auto">
-                <Icon fontSize="large" style={{ color: "#c0a700" }} />
-            </IconButton>
-            <h3 className="text-xl text-contrast dark:text-contrast-dark font-semibold mb-2">
-                {title}
-            </h3>
-            <p className="text-gray-600 dark:text-gray-400 font-light mb-4">
-                {description}
-            </p>
-            <a href="#" className="text-primary dark:text-primary-dark hover:underline">Learn More</a>
+            <Features />
         </div>
     );
 };
