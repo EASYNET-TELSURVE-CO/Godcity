50554c59c9f350e62e0d4e1c181fddad0187829e
Dr-dyrane <halodyrane@gmail.com>
2024-07-24 01:40:31 +0100

    update

--- src/components/home/PaymentSection.jsx
+++ src/components/home/PaymentSection.jsx
@@ -141,7 +141,7 @@ const PaymentSection = () => {
             <thead>
               <tr>
                 <th className="py-2 text-left border-b">Feature</th>
-                <th className="py-2 text-center border-b">Family A</th>
+                <th className="py-2 text-center border-b">Family</th>
                 <th className="py-2 text-center border-b">Premium</th>
                 <th className="py-2 text-center border-b">Enterprise</th>
               </tr>
