47b322262dfb0985d8eac1205fbad24bafdd9304
Dr-dyrane <halodyrane@gmail.com>
2024-07-24 01:11:28 +0100

    add payment and navbot

---
+++ src/components/home/Chatbot.jsx
@@ -0,0 +1,224 @@
+// src/components/home/Chatbot.jsx
+
+import React, { useState, useRef, useEffect } from "react";
+import { IconButton } from "@mui/material";
+import ChatIcon from "@mui/icons-material/Chat";
+import CloseIcon from "@mui/icons-material/Close";
+import "aos/dist/aos.css";
+
+const Chatbot = ({ sectionRefs }) => {
+	const [isOpen, setIsOpen] = useState(false);
+	const [chatHistory, setChatHistory] = useState([]);
+	const [pendingAction, setPendingAction] = useState(null);
+	const messageInputRef = useRef(null);
+
+	const toggleChatbot = () => setIsOpen(!isOpen);
+
+	const handleUserInput = (input) => {
+		const trimmedInput = input.trim().toLowerCase();
+		let response;
+
+		const affirmationKeywords = ["yes", "yeah", "yep", "sure", "okay", "ok"];
+		const negationKeywords = ["no", "nope", "nah"];
+
+		if (pendingAction) {
+			if (affirmationKeywords.some((word) => trimmedInput.includes(word))) {
+				pendingAction();
+				setPendingAction(null);
+				return;
+			} else if (negationKeywords.some((word) => trimmedInput.includes(word))) {
+				response = "Action canceled.";
+				setPendingAction(null);
+			} else {
+				response = "Please respond with a clear yes or no.";
+				setChatHistory((prev) => [
+					...prev,
+					{ type: "user", message: input },
+					{ type: "bot", message: response },
+				]);
+				return;
+			}
+		} else {
+			switch (true) {
+				case trimmedInput.includes("features"):
+					response = "Do you want to navigate to the Features section?";
+					setPendingAction(() => () => {
+						sectionRefs.features.current.scrollIntoView({ behavior: "smooth" });
+						setChatHistory((prev) => [
+							...prev,
+							{ type: "bot", message: "Navigating to Features section." },
+						]);
+					});
+					break;
+				case trimmedInput.includes("cta"):
+					response = "Do you want to navigate to the Call to Action section?";
+					setPendingAction(() => () => {
+						sectionRefs.cta.current.scrollIntoView({ behavior: "smooth" });
+						setChatHistory((prev) => [
+							...prev,
+							{ type: "bot", message: "Navigating to Call to Action section." },
+						]);
+					});
+					break;
+				case trimmedInput.includes("sermon"):
+					response = "Do you want to navigate to the Sermon section?";
+					setPendingAction(() => () => {
+						sectionRefs.sermon.current.scrollIntoView({ behavior: "smooth" });
+						setChatHistory((prev) => [
+							...prev,
+							{ type: "bot", message: "Navigating to Sermon section." },
+						]);
+					});
+					break;
+				case trimmedInput.includes("banner"):
+					response = "Do you want to navigate to the Banner section?";
+					setPendingAction(() => () => {
+						sectionRefs.banner.current.scrollIntoView({ behavior: "smooth" });
+						setChatHistory((prev) => [
+							...prev,
+							{ type: "bot", message: "Navigating to Banner section." },
+						]);
+					});
+					break;
+				case trimmedInput.includes("about us"):
+					response = "Do you want to navigate to the About Us section?";
+					setPendingAction(() => () => {
+						sectionRefs.aboutUs.current.scrollIntoView({ behavior: "smooth" });
+						setChatHistory((prev) => [
+							...prev,
+							{ type: "bot", message: "Navigating to About Us section." },
+						]);
+					});
+					break;
+				case trimmedInput.includes("testimonials"):
+					response = "Do you want to navigate to the Testimonials section?";
+					setPendingAction(() => () => {
+						sectionRefs.testimonials.current.scrollIntoView({
+							behavior: "smooth",
+						});
+						setChatHistory((prev) => [
+							...prev,
+							{ type: "bot", message: "Navigating to Testimonials section." },
+						]);
+					});
+					break;
+				case trimmedInput.includes("events"):
+					response = "Do you want to navigate to the Events section?";
+					setPendingAction(() => () => {
+						sectionRefs.events.current.scrollIntoView({ behavior: "smooth" });
+						setChatHistory((prev) => [
+							...prev,
+							{ type: "bot", message: "Navigating to Events section." },
+						]);
+					});
+					break;
+				case trimmedInput.includes("payment"):
+					response = "Do you want to navigate to the Payment section?";
+					setPendingAction(() => () => {
+						sectionRefs.payment.current.scrollIntoView({ behavior: "smooth" });
+						setChatHistory((prev) => [
+							...prev,
+							{ type: "bot", message: "Navigating to Payment section." },
+						]);
+					});
+					break;
+				case trimmedInput.includes("contact"):
+					response = "Do you want to navigate to the Contact section?";
+					setPendingAction(() => () => {
+						sectionRefs.contact.current.scrollIntoView({ behavior: "smooth" });
+						setChatHistory((prev) => [
+							...prev,
+							{ type: "bot", message: "Navigating to Contact section." },
+						]);
+					});
+					break;
+				default:
+					response = "Sorry, I didn't understand that. Please try again.";
+			}
+		}
+
+		setChatHistory((prev) => [
+			...prev,
+			{ type: "user", message: input },
+			{ type: "bot", message: response },
+		]);
+	};
+
+	useEffect(() => {
+		const darkModeMediaQuery = window.matchMedia(
+			"(prefers-color-scheme: dark)"
+		);
+		const handleDarkModeChange = (e) => {
+			document.documentElement.classList.toggle("dark", e.matches);
+		};
+
+		handleDarkModeChange(darkModeMediaQuery);
+		darkModeMediaQuery.addEventListener("change", handleDarkModeChange);
+
+		return () =>
+			darkModeMediaQuery.removeEventListener("change", handleDarkModeChange);
+	}, []);
+
+	return (
+		<div
+			className={`fixed bottom-[30%] right-4 shadow-2xl rounded-3xl p-8 transition-all text-gray-700 dark:text-gray-300
+        ${
+					isOpen
+						? "w-full sm:w-3/4 md:w-1/2 lg:w-1/3 xl:w-1/4 bg-white dark:bg-gray-800"
+						: "w-16 h-16 bg-primary flex items-center justify-center"
+				}  z-[9999]`}
+			style={{
+				animation: isOpen ? "none" : "bounce 2s infinite",
+			}}
+		>
+			<div className="flex items-center justify-between">
+				<h2
+					className={`text-lg text-black dark:text-white ${
+						isOpen ? "block" : "hidden"
+					}`}
+				>
+					Where do you want to navigate to ?
+				</h2>
+				<IconButton onClick={toggleChatbot} size="large">
+					{isOpen ? (
+						<CloseIcon className="text-primary" />
+					) : (
+						<ChatIcon className="text-white" />
+					)}
+				</IconButton>
+			</div>
+			{isOpen && (
+				<div className="mt-4 flex flex-col h-80">
+					<div className="flex-grow overflow-y-auto mb-2 scrollbar-hidden">
+						{chatHistory.map((entry, index) => (
+							<div
+								key={index}
+								className={`mb-4 p-2 rounded-lg font-light ${
+									entry.type === "bot"
+										? "bg-green-100 text-left dark:bg-green-900 text-green-600 dark:text-green-300 rounded-l-lg rounded-r-2xl"
+										: "bg-blue-100 dark:bg-blue-900 text-blue-600 dark:text-blue-300 rounded-r-lg rounded-l-2xl text-right"
+								}`}
+							>
+								{entry.message}
+							</div>
+						))}
+					</div>
+					<input
+						type="text"
+						placeholder="Type here..."
+						className="w-full p-4 font-light border rounded-2xl dark:bg-gray-700 dark:text-gray-300"
+						onKeyDown={(e) => {
+							if (e.key === "Enter") {
+								handleUserInput(e.target.value);
+								e.target.value = "";
+							}
+						}}
+						ref={messageInputRef}
+					/>
+				</div>
+			)}
+		</div>
+	);
+};
+
+export default Chatbot;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 // src/pages/Home.jsx
 
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import { useAuth } from "../contexts/AuthContext";
 import { useNavigate } from "react-router";
 import AOS from "aos";
@@ -13,70 +13,107 @@ import AboutUs from "../components/home/AboutUs";
 import Testimonials from "../components/home/Testimonials";
 import Events from "../components/home/Events";
 import Contact from "../components/home/Contact";
+import PaymentSection from "../components/home/PaymentSection"; // Import the PaymentSection component
+import Chatbot from "../components/home/Chatbot"; // Import the Chatbot component
 
 const Home = () => {
-    const { user } = useAuth();
-    const navigate = useNavigate();
+	const { user } = useAuth();
+	const navigate = useNavigate();
+	const sectionRefs = {
+		features: useRef(null),
+		cta: useRef(null),
+		sermon: useRef(null),
+		banner: useRef(null),
+		hero: useRef(null),
+		aboutUs: useRef(null),
+		testimonials: useRef(null),
+		events: useRef(null),
+		payment: useRef(null),
+		contact: useRef(null),
+	};
 
-    useEffect(() => {
-        if (user) {
-            navigate("/dashboard");
-        }
-    }, [user, navigate]);
+	useEffect(() => {
+		if (user) {
+			navigate("/dashboard");
+		}
+	}, [user, navigate]);
 
-    useEffect(() => {
-        AOS.init({ duration: 1000 });
-    }, []);
+	useEffect(() => {
+		AOS.init({ duration: 1000 });
+	}, []);
 
-    return (
-        <div>
-            <section
-                className="flex flex-col bg-hero dark:bg-hero_dark bg-cover min-h-screen dark:text-contrast text-center
+	return (
+		<div className="scrollbar-hidden">
+			<section
+				className="flex flex-col bg-hero dark:bg-hero_dark bg-cover min-h-screen dark:text-contrast text-center scrollbar-hidden
                 space-y-4 xs:space-y-6 sm:space-y-8 md:space-y-10 lg:space-y-12 xl:space-y-14 2xl:space-y-16 justify-center items-center py-20"
-            >
-                <div className="bg-white/25 dark:bg-black/25 m-4 p-8 rounded-3xl bg-opacity-25 text-[#000] dark:text-white">
-                    <h1
-                        className="text-3xl xs:text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl 2xl:text-9xl font-bold mb-4"
-                        data-aos="fade-up"
-                    >
-                        Welcome to GodCi†y
-                    </h1>
-                    <p
-                        className="text-base xs:text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl 2xl:text-5xl mb-8"
-                        data-aos="fade-up"
-                        data-aos-delay="100"
-                    >
-                        Unify. Serve. Inspire.
-                    </p>
-                    <p
-                        className="p-10 text-lg xs:text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl 2xl:text-6xl italic relative z-10 font-light"
-                        data-aos="fade-up"
-                        data-aos-delay="200"
-                    >
-                        "For where two or three gather in my name, there am I
-                        with them." – Matthew 18:20
-                    </p>
-                    <button
-                        className="bg-primary text-white text-sm xs:text-base sm:text-lg md:text-xl lg:text-2xl xl:text-3xl 2xl:text-4xl px-4 xs:px-6 sm:px-8 md:px-10 lg:px-12 xl:px-14 2xl:px-16 py-3 sm:py-4 md:py-5 lg:py-6 xl:py-7 2xl:py-8 rounded-full hover:bg-primary-dark transition"
-                        data-aos="fade-up"
-                        data-aos-delay="300"
-                    >
-                        Get Started
-                    </button>
-                </div>
-            </section>
+				ref={sectionRefs.hero}
+               
+			>
+				<div className="bg-white/25 dark:bg-black/25 m-4 p-8 rounded-3xl bg-opacity-25 text-[#000] dark:text-white">
+					<h1
+						className="text-3xl xs:text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl 2xl:text-9xl font-bold mb-4"
+						data-aos="fade-up"
+					>
+						Welcome to GodCi†y
+					</h1>
+					<p
+						className="text-base xs:text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl 2xl:text-5xl mb-8"
+						data-aos="fade-up"
+						data-aos-delay="100"
+					>
+						Unify. Serve. Inspire.
+					</p>
+					<p
+						className="p-10 text-lg xs:text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl 2xl:text-6xl italic relative z-10 font-light"
+						data-aos="fade-up"
+						data-aos-delay="200"
+					>
+						"For where two or three gather in my name, there am I with them." –
+						Matthew 18:20
+					</p>
+					<button
+						className="bg-primary text-white text-sm xs:text-base sm:text-lg md:text-xl lg:text-2xl xl:text-3xl 2xl:text-4xl px-4 xs:px-6 sm:px-8 md:px-10 lg:px-12 xl:px-14 2xl:px-16 py-3 sm:py-4 md:py-5 lg:py-6 xl:py-7 2xl:py-8 rounded-full hover:bg-primary-dark transition"
+						data-aos="fade-up"
+						data-aos-delay="300"
+					>
+						Get Started
+					</button>
+				</div>
+			</section>
 
-            <Features />
-            <Cta />
-            <Sermon />
-            <Banner />
-            <AboutUs />
-            <Testimonials />
-            <Events />
-            <Cta />
-            <Contact />
-        </div>
-    );
+			<section ref={sectionRefs.features}>
+				<Features />
+			</section>
+			<section ref={sectionRefs.cta}>
+				<Cta />
+			</section>
+			<section ref={sectionRefs.sermon}>
+				<Sermon />
+			</section>
+			<section ref={sectionRefs.banner}>
+				<Banner />
+			</section>
+			<section ref={sectionRefs.aboutUs}>
+				<AboutUs />
+			</section>
+			<section ref={sectionRefs.testimonials}>
+				<Testimonials />
+			</section>
+			<section ref={sectionRefs.events}>
+				<Events />
+			</section>
+			<section ref={sectionRefs.payment}>
+				<PaymentSection />
+			</section>
+			<section ref={sectionRefs.contact}>
+				<Contact />
+			</section>
+
+			{/* Add the Chatbot component */}
+			<Chatbot sectionRefs={sectionRefs} />
+		</div>
+	);
 };
 
 export default Home;
