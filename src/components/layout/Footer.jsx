e6af36f7a5b59e164c78359e130901cfde7a3c52
EASYNETY <israeladeyeyee@gmail.com>
2024-08-26 01:22:06 +0100

    New Dasboard Url added

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -43,7 +43,7 @@ const Footer = () => {
 							rel="noopener noreferrer"
 							className="hover:underline ml-1"
 						>
-							Designed by Easynet.
+							© 2024, Designed by Iyayi Osa Ojogan.
 						</a>
 					</div>
 					<DarkModeToggle />
